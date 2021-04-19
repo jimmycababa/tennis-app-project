dff16c1bd89dfe2742ea4ab055dd39a8ce1b44c4
jimmycababa <jimmycababa@gmail.com>
2021-04-19 16:18:20 -0500

    added ejs file and gitignore file

--- server.js
+++ server.js
@@ -4,13 +4,25 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 
 let db,
-    dbConnectionStr = 'mongodb+srv://demo:demo@cluster0.lxhnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    dbConnectionStr = 'mongodb+srv://demo:demo@cluster0.lxhnr.mongodb.net/tennis-app-project?retryWrites=true&w=majority',
      dbName = 'tennisCourts'
 
      MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
      .then(client => {
          console.log(`connected to ${dbName} database`)
          db = client.db(dbName)
+     })
+     .catch(err => {
+         console.log(err)
+     })
+
+     app.set('view engine', 'ejs' )
+     app.use(express.static('public'))
+     app.use(express.urlencoded({ extended: true}))
+     app.use(express.json())
+
+     app.get('/', (req, res) => {
+        res.render('index.ejs')
      })
 
      app.listen(PORT, () =>{
